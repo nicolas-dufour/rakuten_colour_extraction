44cd11880262b59c8694a66148f0e13a62b75b0e
unknown <tom.huix@supelec.fr>
2021-03-03 09:13:27 +0100

    labels file + bert dataset

--- data/bert.py
+++ data/bert.py
@@ -1,41 +1,27 @@
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer
+from labels import Labels
 import pandas as pd
 import numpy as np 
 import torch
 
 class Bert_dataset(Dataset):
-    def __init__(self, X_path, y_path, max_len):
-      self.sentences = pd.read_csv(X_path)['item_caption']
-      correct_values = self.sentences.notna()
-      self.sentences = self.sentences[correct_values].values
+    def __init__(self, data_path, X_path, y_path, max_len):
+      self.labels_obj = Labels(data_path + y_path)
       self.tokenizer = BertTokenizer.from_pretrained('cl-tohoku/bert-base-japanese')
       self.max_len = max_len
-      self.colors_dict = self.init_color_idx(y_path)
-      self.targets = self.create_target_labels(y_path, self.colors_dict)
-      self.targets = self.targets[correct_values, :]
+      self.load_datasets(X_path, y_path)
+      self.remove_empty_description()
 
-    def init_color_idx(self, y_path):
-      colors = {}
-      df = pd.read_csv(y_path)
-      for _, row in df.iterrows():
-        color_list = self.str_to_list(row['color_tags'])
-        for color in color_list:
-          colors.setdefault(color, len(colors))
-      return colors
+    def load_datasets(self, X_path, y_path):
+      self.sentences = pd.read_csv(data_path + X_path)['item_caption']
+      self.labels, self.one_hot_labels, self.classes = self.labels_obj.load()
 
-    def create_target_labels(self, y_path, colors_dict):
-      df = pd.read_csv(y_path)
-      targets = np.zeros((len(df), len(colors_dict)))
-      for idx, (_, row) in enumerate(df.iterrows()):
-        color_list = self.str_to_list(row['color_tags'])
-        for color in color_list:
-          targets[idx, colors_dict[color]] = 1
-      return targets
-
-    def str_to_list(self, s):
-      s = s.replace('[', '').replace(']', '').replace(',', '')
-      return s.split()
+    def remove_empty_description(self):
+      correct_descriptions = self.sentences.notna()
+      self.sentences = self.sentences[correct_descriptions].values
+      self.labels = self.labels[correct_descriptions, :]
+      self.one_hot_labels = self.one_hot_labels[correct_descriptions, :]
 
     def __len__(self):
         return len(self.sentences)
@@ -56,4 +42,4 @@ class Bert_dataset(Dataset):
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
             'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(self.targets[idx], dtype=torch.float)}
+            'targets': torch.tensor(self.one_hot_labels[idx], dtype=torch.float)}
---
+++ data/labels.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
+
+class Labels():
+    def __init__(self,y_path):
+        self.mlb = MultiLabelBinarizer()
+        self.y_path = y_path
+
+    def load(self):
+        self.labels = pd.read_csv(self.y_path, indox_col=0)
+        self.labels = self.labels['color_tags'.apply(self.str_to_list)]
+        self.onehot_labels = self.mlb.fit_transform(self.labels)
+        self.classes_correp = self.mlb.classes_
+        return self.labels, self.onehot_labels, self.classes_correp
+
+    def str_to_list(self, s):
+      s = s.replace('[', '').replace(']', '').replace(',', '')
+      return s.split()
