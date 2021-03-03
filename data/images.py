41ede58f2c77a52f315bcb7d627922cf2638ec65
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-03-03 13:56:54 +0000

    Fixed Image Loading

--- data/images.py
+++ data/images.py
@@ -2,7 +2,10 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np 
 import torch
-from PIL import Image
+from PIL import Image, ImageSequence
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class ImageDataset(Dataset):
     def __init__(self, image_paths, image_folder, labels, transform=None):
@@ -12,10 +15,14 @@ class ImageDataset(Dataset):
         self.transform = transform
     def __getitem__(self, idx):
         im_name = self.paths.iloc[idx]
-        image = np.array(Image.open(self.image_folder+im_name))
+        im = Image.open(self.image_folder+im_name)
+        if(im_name.split('.')[-1]=='gif'):
+            image = ImageSequence.Iterator(im)[0].convert('RGB')
+        else:
+            image = im.convert('RGB')
         label = self.labels[idx]
         if self.transform:
             image = self.transform(image)
-        return image, label
+        return torch.FloatTensor(image), torch.FloatTensor(label)
     def __len__(self):
         return len(self.paths)
