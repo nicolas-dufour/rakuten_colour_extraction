a8aa24385aded202503a7f4b4e05aee9111d8b59
Nicolas DUFOUR <nicolas.dufourn@gmail.com>
2021-03-04 14:14:12 +0000

    Fixed Label Onehot

--- data/labels.py
+++ data/labels.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
+import ast
 
 class Labels:
     def __init__(self,y_path):
@@ -8,11 +9,7 @@ class Labels:
 
     def load(self):
         self.labels = pd.read_csv(self.y_path, index_col=0)
-        self.labels = self.labels['color_tags'].apply(self.str_to_list)
+        self.labels = self.labels['color_tags'].apply(ast.literal_eval)
         self.onehot_labels = self.mlb.fit_transform(self.labels)
         self.classes_correp = self.mlb.classes_
         return self.labels, self.onehot_labels, self.classes_correp
-
-    def str_to_list(self, s):
-      s = s.replace('[', '').replace(']', '').replace(',', '')
-      return s.split()
