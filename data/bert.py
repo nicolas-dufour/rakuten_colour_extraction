ce1454e599dd1932977ba95a79ba85c31b80370a
unknown <tom.huix@supelec.fr>
2021-03-07 12:26:34 +0100

    dynamic padding

--- data/bert.py
+++ data/bert.py
@@ -5,24 +5,54 @@ import pandas as pd
 import numpy as np 
 import torch
 from torch.utils.data import random_split
+import random
+from torch.nn.utils.rnn import pad_sequence
 
+class Data:
+  def __init__(self, t, o):
+    self.text = t
+    self.label = o
+
+class Features:
+  def __init__(self, ids, mask, token_type_ids, target):
+    self.ids = ids
+    self.mask = mask
+    self.token_type_ids = token_type_ids
+    self.target = target
+  
 class Bert_dataset(Dataset):
-  def __init__(self, data_path, X_path, y_path, max_len):
+  def __init__(self, data_path, X_path, y_path, max_len, batch_size):
+    self.batch_size = batch_size
     self.labels_obj = Labels(data_path + y_path)
     self.tokenizer = BertTokenizer.from_pretrained('cl-tohoku/bert-base-japanese')
     self.max_len = max_len
-    self.load_datasets(data_path, X_path, y_path)
-    self.remove_empty_description()
+    self.batches = self.manage_data(data_path, X_path, y_path)
+
+  def manage_data(self, data_path, X_path, y_path):
+    text_df, one_hot_labels, self.classes = self.load_datasets(data_path, X_path, y_path)
+    data = self.remove_empty_description(text_df, one_hot_labels)
+    data = self.sort_data(data)
+    batches = self.build_batches(data)
+    return batches
+
+  def sort_data(self, data):
+    data.sort(key=lambda x: x[0])
+    return [x[1] for x in data]
 
   def load_datasets(self, data_path, X_path, y_path):
     X = pd.read_csv(data_path + X_path)
-    self.texts = self.preprocess_text(X['item_caption'], X['item_name'], X)
-    self.labels, self.one_hot_labels, self.classes = self.labels_obj.load()
+    text_df = self.preprocess_text(X['item_caption'], X['item_name'], X)
+    _, one_hot_labels, classes = self.labels_obj.load()
+    return text_df, one_hot_labels, classes
 
-  def remove_empty_description(self):
-    correct_texts = self.texts.notna()
-    self.texts = self.texts[correct_texts].values
-    self.one_hot_labels = self.one_hot_labels[correct_texts, :]
+  def remove_empty_description(self, text_df, one_hot_labels):
+    correct_texts = text_df['text'].notna()
+    text_df = text_df[correct_texts]
+    text_df['sizes'] = text_df['text'].apply(len)
+    sizes = text_df.sizes.values
+    texts = text_df['text'].values
+    one_hot_labels = one_hot_labels[correct_texts, :]
+    return [(s, Data(t, l)) for t, s, l in zip(texts, sizes, one_hot_labels)]
 
   def preprocess_text(self, sentences, titles, X):
     correct_descriptions = sentences.notna()
@@ -37,58 +67,52 @@ class Bert_dataset(Dataset):
     X['text'].loc[unique_description] = text_description
     X['text'].loc[unique_title] = text_title
     X['text'].loc[both] = text_both
-    return X['text']
+    return X
+
+  def build_batches(self, data):
+    batch_ordered_sentences = []
+    while len(data) > 0:
+        to_take = min(self.batch_size, len(data))
+        select = random.randint(0, len(data) - to_take)
+        batch_ordered_sentences += data[select:select + to_take]
+        del data[select:select + to_take]
+    return batch_ordered_sentences
+
+  def encode(self, batch):
+    inputs = self.tokenizer.encode_plus(batch.text,
+                                        None,
+                                        add_special_tokens=True,
+                                        max_length=self.max_len,
+                                        padding=False,
+                                        return_token_type_ids=True,
+                                        return_attention_mask=True,
+                                        return_overflowing_tokens=False,
+                                        return_special_tokens_mask=False)
+    return Features(ids=inputs['input_ids'],
+                    mask = inputs['attention_mask'],
+                    token_type_ids = inputs["token_type_ids"],
+                    target = batch.label)
 
   def get_nb_classes(self):
     return len(self.classes)
 
   def __len__(self):
-    return len(self.texts)
+    return len(self.batches)
 
   def __getitem__(self, idx):
-    sentence = self.texts[idx]
-    inputs = self.tokenizer.encode_plus(
-      sentence,
-      None,
-      add_special_tokens=True,
-      max_length=self.max_len,
-      padding='max_length',
-      return_token_type_ids=True,
-      truncation=True)
-    ids = inputs['input_ids']
-    mask = inputs['attention_mask']
-    token_type_ids = inputs["token_type_ids"]
-    return {
-        'ids': torch.tensor(ids, dtype=torch.long),
-        'mask': torch.tensor(mask, dtype=torch.long),
-        'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-        'targets': torch.tensor(self.one_hot_labels[idx], dtype=torch.float)}
+    batch = self.batches[idx]
+    return self.encode(batch)
 
-
-
-# def pad_seq(seq: List[int], max_batch_len: int, pad_value: int) -> List[int]:
-#     # IRL, use pad_sequence
-#     # https://pytorch.org/docs/master/generated/torch.nn.utils.rnn.pad_sequence.html
-#     return seq + (max_batch_len - len(seq)) * [pad_value]@dataclass
-
-# class SmartCollator(DataCollator):
-#     pad_token_id: int    
-#     def collate_batch(self, batch: List[Features]) -> Dict[str, torch.Tensor]:
-#         batch_inputs = list()
-#         batch_attention_masks = list()
-#         labels = list()
-#         # find the max length of the mini batch
-#         max_size = max([len(ex.input_ids) for ex in batch])
-#         for item in batch:
-#             # apply padding at the mini batch level
-#             batch_inputs += [pad_seq(item.input_ids, max_size, self.pad_token_id)]
-#             batch_attention_masks += [pad_seq(item.attention_mask, max_size, 0)]
-#             labels.append(item.label)
-#         # expected Transformers input format (dict of Tensors)
-#         return {"input_ids": torch.tensor(batch_inputs, dtype=torch.long),
-#                 "attention_mask": torch.tensor(batch_attention_masks, dtype=torch.long),
-#                 "labels": torch.tensor(labels, dtype=torch.long)
-#                 }
+def collate_batch(batch):
+    batch_inputs, batch_attention_masks, labels, batch_token_type_ids = list(), list(), list(), list()
+    ids = pad_sequence([torch.tensor(data.ids) for data in batch], 0)
+    mask = pad_sequence([torch.tensor(data.mask) for data in batch], 0)
+    token_type = pad_sequence([torch.tensor(data.token_type_ids) for data in batch], 0)
+    return {"input_ids": torch.tensor(batch_inputs, dtype=torch.long),
+            "attention_mask": torch.tensor(batch_attention_masks, dtype=torch.long),
+            "token_type_ids": torch.tensor(batch_token_type_ids, dtype=torch.long),
+            "labels": torch.tensor(labels, dtype=torch.long)
+            }
 
 
 class Bert_Data:
@@ -113,16 +137,19 @@ class Bert_Data:
     print('[SYSTEM]Test size', test.__len__())
 
   def build(self):
-      dataset = Bert_dataset(self.data_path, self.X_path, self.y_path, self.MAX_LEN)
+      dataset = Bert_dataset(self.data_path, self.X_path, self.y_path, self.MAX_LEN, self.batch_size)
       train_dataset, val_dataset, test_dataset = self.split_dataset(dataset)
       self.plot_sizes(train_dataset, val_dataset, test_dataset)
 
       train_loader = DataLoader(train_dataset, batch_size=self.batch_size,
-                                shuffle=True, num_workers=self.workers)
+                                shuffle=True, num_workers=self.workers, 
+                                collate_fn=collate_batch)
 
       val_loader = DataLoader(val_dataset, batch_size=self.batch_size,
-                              shuffle=False, num_workers=self.workers)
+                              shuffle=False, num_workers=self.workers,
+                              collate_fn=collate_batch)
 
       test_loader = DataLoader(test_dataset, batch_size=self.batch_size,
-                               shuffle=False, num_workers=self.workers)
+                               shuffle=False, num_workers=self.workers, 
+                               collate_fn=collate_batch)
       return train_loader, val_loader, test_loader, dataset.get_nb_classes()
